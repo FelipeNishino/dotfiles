88810c09a7c8fed25c2dde0dff1daabfe93f78d5
FelipeNishino <felipenishino@gmail.com>
2022-05-16 20:33:20 -0300

    Fixed copy_from_repo script

--- copy_from_repo.sh
+++ copy_from_repo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cp -r .Xresources .xinitrc .zshrc .zshenv .icons/ .dwm/ .scripts/ ~
-cp -r .oh-my-zsh/custom/themes ~/.oh-my-zsh/custom/themes
+cp -r .oh-my-zsh/custom/themes ~/.oh-my-zsh/custom/
