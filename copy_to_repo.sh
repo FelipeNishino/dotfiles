f93810023eb8a17d3bb9bc6172f6b76a4bfdfad1
FelipeNishino <felipenishino@gmail.com>
2022-05-21 05:37:36 -0300

    Added rofi configuration files

--- copy_to_repo.sh
+++ copy_to_repo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cp -r ~/.Xresources ~/.xinitrc ~/.zshrc ~/.zshenv ~/.icons/ ~/.dwm/ ~/.scripts/ .
+cp -r ~/.Xresources ~/.xinitrc ~/.zshrc ~/.zshenv ~/.icons/ ~/.dwm/ ~/.scripts/ ~/.config/rofi .
 cp -r ~/.oh-my-zsh/custom/themes .oh-my-zsh/custom/
