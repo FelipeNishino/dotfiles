11018acff6d106e33b991a134a89f5a9a4fb3f55
FelipeNishino <felipenishino@gmail.com>
2022-05-21 05:53:26 -0300

    Fixed copy_to_repo.sh script

--- copy_to_repo.sh
+++ copy_to_repo.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cp -r ~/.Xresources ~/.xinitrc ~/.zshrc ~/.zshenv ~/.icons/ ~/.dwm/ ~/.scripts/ ~/.config/rofi .
+cp -r ~/.Xresources ~/.xinitrc ~/.zshrc ~/.zshenv ~/.icons/ ~/.dwm/ ~/.scripts/ .
 cp -r ~/.oh-my-zsh/custom/themes .oh-my-zsh/custom/
+cp -r ~/.config/rofi .config/
