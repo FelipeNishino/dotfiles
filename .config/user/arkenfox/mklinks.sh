c8901517f29c79fafcf7aae8b0c3ae0d38de147c
Felipe Nishino <nishino.dev@pm.me>
2025-02-11 19:46:55 -0300

    Make arkenfox helper script support macos and linux

--- .config/user/arkenfox/mklinks.sh
+++ .config/user/arkenfox/mklinks.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+function abort() {
+    echo "<Abort>: ${@}"
+    exit 1
+}
 
 function check_file() {
     if [ -z "$1" ]; then
@@ -13,18 +17,23 @@ function check_file() {
 }
 
 function main() {
-    profiledirs=$(ls -d */ | grep "\.af" | cut -f1 -d'/')
-
-    for pdir in $profiledirs;
+    case "$OSTYPE" in
+        darwin*) ffdir="$HOME/Library/Application Support/Firefox/Profiles";;
+        linux-gnu*) ffdir="$HOME/.mozilla/firefox";;
+        *) abort "Couldn't match ostype, got <$OSTYPE>";;
+    esac
+    pdirs=$(ls -F "$ffdir" | grep "/$" | grep "\.af" | cut -f1 -d'/')
+    afuserdir="$HOME/.config/user/arkenfox"
+    for pdir in $pdirs;
     do
-        uov="/home/nishi/.config/user/arkenfox/$(echo $pdir | cut -f2 -d'.').user-overrides.js"
-        updater="/home/nishi/.config/user/arkenfox/updater.sh"
-        cleaner="/home/nishi/.config/user/arkenfox/prefsCleaner.sh"
+        uov="$afuserdir/$(echo $pdir | cut -f2 -d'.').user-overrides.js"
+        updater="$afuserdir/updater.sh"
+        cleaner="$afuserdir/prefsCleaner.sh"
         #check_file($uov) && ln -s $uov ./$pdir/user-overrides.js
         #check_file $uov && echo "created link for useroverride"
         #check_file $updater && echo "created link for updater"
         #check_file $cleaner && echo "created link for cleaner"
-        check_file $uov && ln -s $uov ./$pdir/user-overrides.js
+        check_file $uov && ln -s $uov "$ffdir/$pdir/user-overrides.js"
         #check_file $updater && ln -s $updater ./$pdir/updater.sh
         #check_file $cleaner && ln -s $cleaner ./$pdir/prefsCleaner.sh
     done
